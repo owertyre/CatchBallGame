cc8dedca91647814e3ffc0f3b9b5c6c9f39f0307
Alexandr Denisov <C:\Users\alex_\AppData\Roaming\The Bat! Pwd>
2021-12-20 21:14:46 +0300

    realese v2

---
+++ Assets/Scripts/RestartButton.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartButton : MonoBehaviour
+{
+    public void RestartGame(){
+
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1;
+    }
+  
+}
